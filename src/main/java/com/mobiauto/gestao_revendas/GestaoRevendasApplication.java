ea0f1619661c797a0417eb63dff536e72929cdcc
Victor <victormota_03@hotmail.com>
2025-04-09 08:50:05 -0300

    First Commit

---
+++ src/main/java/com/mobiauto/gestao_revendas/GestaoRevendasApplication.java
@@ -0,0 +1,13 @@
+package com.mobiauto.gestao_revendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoRevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoRevendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobiauto/gestao_revendas/GestaoRevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiauto.gestao_revendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoRevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
